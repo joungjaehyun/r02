78e7e6d943cb84cba3e024aecd2841b3dd09d3cc
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-05 15:41:24 +0900

    Update root.js

--- src/routers/root.js
+++ src/routers/root.js
@@ -1,11 +1,15 @@
-const { createBrowserRouter } = require("react-router-dom");
-const { default: MainPage } = require("../pages/MainPage");
-
+import { createBrowserRouter } from"react-router-dom";
+import MainPage from "../pages/MainPage";
+import AboutPage from "../pages/AboutPage";
 
 
 const router = createBrowserRouter([
     {
         path:"/",
         elememt: <MainPage></MainPage>
+    },
+    {
+        path: "/about",
+        element: <AboutPage></AboutPage>
     }
 ])
