e256482f37064925e933ade159f94017b503eb3b
Seongwhwan Park <joungjaehyun1008@gmail.com>
2023-07-06 16:24:13 +0900

    Pagination

--- src/components/board/ListComponent.js
+++ src/components/board/ListComponent.js
@@ -1,11 +1,26 @@
 
 //실제 axios에  전달하고 responseDTO를 받아서 처리해주는 Component
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { createSearchParams } from "react-router-dom";
 import { getList } from "../../api/boardAPI";
 
-const ListComponent = ({queryObj}) => {
+const initState = {
+    dtoList:[],
+    end:0,
+    start:0,
+    next:false,
+    prev:false,
+    pageNums:[],
+    page:0,
+    size:0,
+    requestDTO: null
+}
+
+
+const ListComponent = ({queryObj,movePage}) => {
+    // rendering시 에러방지
+    const [listData, setListData] = useState(initState)
 
     // use가 붙으면 component 에서만 쓸수있다.
     // 그외는 자체적으로 제공하는 함수라 생각한다.
@@ -13,13 +28,41 @@ const ListComponent = ({queryObj}) => {
 
         getList(queryObj).then(data =>{
             console.log(data)
+            setListData(data)
         })
 
-    },[{...queryObj}])
+    },[queryObj])
+
+    const handleClickPage = (pageNum) =>{
+        movePage(pageNum)
+    }
     
     return (  
         <div>
             <div>List Component</div>
+            <div>
+                <ul>
+                    {listData.dtoList.map(
+                     ({bno,title,replyCount})   => <li key={bno}>{bno} - {title}
+                    - [{replyCount}]</li>)}
+                </ul>
+            </div>
+            <div className="m-4 p-2">
+                {/* 페이지클릭시 movePage 호출이 되는지 || 파라미터 갯수들이 얼마나 필요한지  */}
+                <ul className="flex">
+                    {listData.prev ? <li className="m-2 p-2 bg-blue-500 border-2 text-white font-bold rounded-lg"
+                    onClick={()=> handleClickPage(listData.start-1)}
+                    >PREV</li>:<></>}
+                    {listData.pageNums.map(num => <li 
+                    className="m-2 p-2 bg-blue-500 border-2 text-white font-bold rounded-lg"
+                      key={num}
+                      onClick={()=>handleClickPage(num)}
+                      >{num}</li>)}
+                    {listData.next ? <li className="m-2 p-2 bg-blue-500 border-2 text-white font-bold rounded-lg"
+                    onClick={()=> handleClickPage(listData.end+1)}
+                    >NEXT</li>:<></>}     
+                </ul>
+            </div>
         </div>
     );
 }
--- src/pages/board/ListPage.js
+++ src/pages/board/ListPage.js
@@ -1,33 +1,60 @@
 import { useSearchParams } from "react-router-dom";
 import ListComponent from "../../components/board/ListComponent";
 
+
+const checkNull = (obj) => {
+
+    const result = {}
+
+    for (const attr in obj) {
+        const attrName = attr
+        const attrValue = obj[attr]
+
+        if (attrValue && attrValue !== 'null') {
+            result[attrName] = attrValue
+        }
+    }
+
+    return result
+}
+
+
+
 const ListPage = () => {
 
     // Query String 처리
-    const [search, setSearch]= useSearchParams()
-    
+    const [search, setSearch] = useSearchParams()
+
     console.log(search)
     // page size 값은 없다면 초기값 설정
-    const page = search.get("page")|| 1
+    const page = search.get("page") || 1
     const size = search.get("size") || 10
     const type = search.get("type")
     const keyword = search.get("keyword")
 
     // object로  묶어주기
-    const queryObj = {page,size,type,keyword}
+    const queryObj = checkNull({ page, size, type, keyword })
 
 
-    console.log("queryObj---------------- " )
+    console.log("queryObj---------------- ")
     console.log(queryObj)
 
     console.log("Board List go")
-    return ( 
-            <div>
-                {/* queryobj를 전달 해준다.  */}
-                Board List Page
-                <ListComponent queryObj={queryObj}></ListComponent>
-            </div>
-     );
+
+    const movePage = (num) => {
+        console.log("NUM---------------------------- " + num)
+        // URL 변경시 useNavigate, setSearch 
+        queryObj.page = num
+        setSearch({ ...queryObj })
+    }
+
+    return (
+        <div>
+            {/* queryobj를 전달 해준다.  */}
+            Board List Page
+            <ListComponent queryObj={queryObj} movePage={movePage}></ListComponent>
+        </div>
+    );
 }
- 
+
 export default ListPage;
